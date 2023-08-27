cd2159d1c423acf5f484a1aa4481348fb507b89f
ashleehee <141728803+ashleehee@users.noreply.github.com>
2023-08-27 19:58:45 +0300

    Making different themes

--- vector/src/main/java/im/vector/app/features/settings/VectorPreferences.kt
+++ vector/src/main/java/im/vector/app/features/settings/VectorPreferences.kt
@@ -334,6 +334,7 @@ class VectorPreferences @Inject constructor(
 
         // theme
         keysToKeep.add(ThemeUtils.APPLICATION_THEME_KEY)
+        keysToKeep.add(ThemeUtils.APPLICATION_ACCENTS_KEY)
 
         // get all the existing keys
         val keys = defaultPrefs.all.keys
